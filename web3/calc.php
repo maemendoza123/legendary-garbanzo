f7c4aa23d982697fae800d960b3e08c310eb378e
lebBitanga <201980321@psu.palawan.edu.ph>
2022-03-08 15:02:16 +0800

    done web3 & web5

--- web3/calc.php
+++ web3/calc.php
@@ -2,13 +2,31 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+
+        $val1=$_POST['val1'];
+        $val2=$_POST['val2'];
+       
+
         # test if input is_numeric
-        if(1){
+        if(is_numeric($val1) && is_numeric($val2)){
+
             # test if calc is not null
-            if(1){
+            $operators=$_POST['calc'];
+            if(!empty($operators)){
+                 
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($operators){
+                    case "add":
+                        $result =  $val1+$val2;
+                        break;
+                    case "sub":
+                        $result = $val1-$val2;
+                        break;
+                    case "mul":
+                        $result = $val1*$val2;
+                        break;
+                    case "div":
+                        $result = $val1/$val2;
                 }
                 echo "Calculation result: $result";
             }
--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,32 @@
-<?php
+<html>
+<head><title>Calculation Result</title></head>
+<body>
+    <?php
 
+session_start();
+
+$number_visits = $_SESSION['page_load_count'];
+if(isset($_GET['reset']) || !isset($number_visits)){
+    $number_visits = 0;
+}
+$number_visits++;
+$_SESSION['page_load_count'] = $number_visits;
 ?>
-
 <h1>Hello</h1>
-<p>You have visited this page <?= $num_visits ?> times</p>
+<p>You have visited this page <?= $number_visits ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
+
+<?php
+ if($number_visits == 5): ?>
+    <h3>Wow!</h3>
+    <p>You sure come to this page a lot.</p>
+<?php endif ?>
+
+<?php
+ if($number_visits == 10): 
+   header("Location:http://localhost/Phpmyadmin/web5/congratulations.php/");
+   ?>
+    <?php endif ?>
+
+</body>
+</html>
