3f262312403d340b5b00f72806b8415941fca26f
FrancisMartinez2022 <201840137@psu.palawan.edu.ph>
2022-03-08 13:02:35 +0800

    message

--- web2/fav.php
+++ web2/fav.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
-<head><title>Your Favorite</title></head>
+<html>
+    <head>
+        <title>Your Favorite</title>
+    </head>
 <body>
-
 <?php
-echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
-?> 
+        if(isset($_POST["username"]) && ($_POST["dish"])) {
+            echo "Thanks for your selection ".($_POST["username"]).'<br>';
+            echo "You really enjoyed ".($_POST["dish"]).'<br>';
+            echo "-especially with a ".($_POST["color"])." wine";
+            
+        }
+        if(isset($_POST["username"]) && empty($_POST["dish"])) {
+            echo "Unfortunately, you had leave your favorite dish empty". '<br>';
+            echo "Please fill needed details in the empty selection";
+        }
 
 
+    ?>
+
+
+    </form>
 </body>
 </html>
